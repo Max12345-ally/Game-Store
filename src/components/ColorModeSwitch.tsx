e6922d099b3a26e8d05736be6b97cf825f34df20
maxbond <ceo@rf.ru>
2023-04-10 16:29:04 +0300

    bug fixed

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -5,7 +5,7 @@ const ColorModeSwitch = () => {
   const {toggleColorMode, colorMode} = useColorMode();
 
   return (
-    <HStack justifyContent='space-between' padding='10px'>
+    <HStack>
       <Switch colorScheme='blue' isChecked={colorMode === 'dark'} onChange={toggleColorMode}/>
       <Text>Dark Mode</Text>
     </HStack>
