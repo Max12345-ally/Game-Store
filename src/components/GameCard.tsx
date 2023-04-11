a20190acb28e32ad659d8faa3debae22536d1292
maxbond <ceo@rf.ru>
2023-04-11 21:48:46 +0300

    components added

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,5 +1,7 @@
-import { Card, CardBody, Image, Heading, Text } from '@chakra-ui/react';
+import { Card, CardBody, Image, Heading, HStack } from '@chakra-ui/react';
 import { Game } from '../hooks/useGames';
+import getCroppedImageUrl from '../services/image-url';
+import CriticScore from './CriticScore';
 import PlatformIconsList from './PlatformIconsList';
 
 
@@ -10,12 +12,15 @@ interface Props {
 const GameCard = ({ game }: Props) => {
 
   return (
-    <Card borderRadius={10} overflow="hidden">
+    <Card>
         <Image src={game.background_image} />
         <CardBody>
             <Heading fontSize="2xl">{game.name}</Heading>
              {/* {game.parent_platforms.map(({platform}) => <Text>{platform.name}</Text>)} */}
-            <PlatformIconsList platforms={game.parent_platforms.map(p => p.platform)}  />
+             <HStack justifyContent='space-between'>
+              <PlatformIconsList platforms={game.parent_platforms.map(p => p.platform)} />
+              <CriticScore score={game.metacritic} />
+             </HStack>
         </CardBody>
     </Card>
   )
